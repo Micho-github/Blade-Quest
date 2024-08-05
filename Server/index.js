d1f751d10c0812af0f8df565e52368a091dc5f28
Michel khairallah <michelkhairallah67@outlook.com>
2024-08-05 16:17:32 +0300

    fix v3

--- Server/index.js
+++ Server/index.js
@@ -1,25 +1,33 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser')
-
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 //express app
 const app = express();
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Database Connected'))
-.catch((err) => console.log('Database not Connected:', err));
+// Use CORS middleware
+app.use(
+  cors({
+    origin: "https://bladequest.vercel.app",
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Database Connected"))
+  .catch((err) => console.log("Database not Connected:", err));
 
 //middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/', require('./routes/authRouters'))
-
+app.use("/", require("./routes/authRouters"));
 
 // listen for requests
 const port = 8000;
