c992943ced16e9aa0b2d7e37ef4a0461664e4636
Michel khairallah <michelkhairallah67@outlook.com>
2024-08-05 12:14:06 +0300

    ...

--- Client/app/src/App.jsx
+++ Client/app/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./components/Structure/Footer";
 import Header from "./components/Structure/Header";
