142f124a73de4a07681e422679d0c41898c2eb3c
GN1370 <127950903+GN1370@users.noreply.github.com>
2024-08-21 16:47:12 +0300

    small fix

--- Client/app/src/components/Modals/GamePageModals/OptionsModals/HowToPlayModal.jsx
+++ Client/app/src/components/Modals/GamePageModals/OptionsModals/HowToPlayModal.jsx
@@ -8,27 +8,30 @@ export default function HowToPlayModal({ onClose }) {
       <div className={styles.howToPlayModal}>
         <FaChevronLeft className={styles.backIcon} onClick={onClose} />
         <h1>How To Play?</h1>
-        <h2>~ Using a Touch Screen:</h2>
-        <ul>
-          <li>Touch the left side of the Game screen to Hit left.</li>
-          <li>Touch the right side of the Game screen to Hit right.</li>
-          <li>
-            Touch and Hold with two fingers on the game screen to Dodge attacks.
-          </li>
-        </ul>
-        <hr></hr>
-        <h2>~ Using a Mouse and a Keyboard:</h2>
-        <ul>
-          <li>
-            Click on the left side of the Game screen with left click to Hit
-            left.
-          </li>
-          <li>
-            Click on the right side of the Game screen with left click to Hit
-            right.
-          </li>
-          <li>Press and Hold the Space Bar to Dodge attacks. </li>
-        </ul>
+        <div className={styles.howToPlayModal_text}>
+          <h2>~ Using a Touch Screen:</h2>
+          <ul>
+            <li>Touch the left side of the Game screen to Hit left.</li>
+            <li>Touch the right side of the Game screen to Hit right.</li>
+            <li>
+              Touch and Hold with two fingers on the game screen to Dodge
+              attacks.
+            </li>
+          </ul>
+          <hr></hr>
+          <h2>~ Using a Mouse and a Keyboard:</h2>
+          <ul>
+            <li>
+              Click on the left side of the Game screen with left click to Hit
+              left.
+            </li>
+            <li>
+              Click on the right side of the Game screen with left click to Hit
+              right.
+            </li>
+            <li>Press and Hold the Space Bar to Dodge attacks. </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
